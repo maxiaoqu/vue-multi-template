0d1b778004bf3018f3d3db8f759ab148f068016d
maxiaoqu <maxiaoqu@163.com>
2020-05-04 08:49:16 +0800

    自动生成环境变量

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 const consoleInfo = require('./console');
+const fs = require('fs');
+
+// 获取当前环境变量
+var projectName = process.argv[5] || process.argv[3];
+
+// 生成目前运行的环境、状态
+fs.open('./src/environment/nodeEnv.js', 'w', function (err, fd) {
+    const buf = `export const nodeEnv = {
+    projectName: '${projectName}',
+    nodeEnv: '${process.env.NODE_ENV}',
+    currentMode: '${process.env.VUE_APP_CURRENTMODE}',
+    currentModeName: '${process.env.VUE_APP_CURRENTMODE}',
+    currentModeChName: '${process.env.VUE_APP_CURRENTMODE}'
+};`;
+    fs.write(fd, buf, 0, buf.length, 0);
+});
 
 module.exports = {
     publicPath: './',
