2bdc740512646863ecf5568c5fff7ad9c86b8a1b
maxiaoqu <maxiaoqu@163.com>
2020-05-05 14:58:23 +0800

    fetch封装的准备工作

---
+++ src/request/fetch/intercept.js
@@ -0,0 +1,10 @@
+/**
+ * @author maxiaoqu(码小趣：maxiaoqu@163.com)
+ * @since 2019-08-26
+ * @description 添加请求拦截、响应拦截
+ */
+import Fetch from 'axios';
+
+const intercept = Fetch;
+
+export default intercept
